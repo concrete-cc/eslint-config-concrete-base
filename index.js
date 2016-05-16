2928bfb94d72afe9618bf205bf6161ed07691394
lazlojuly <lazlo.gy@gmail.com>
2016-05-17 00:42:08 +0100

    Extending Airbnb's extensible shared config

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "no-use-before-define": [2, {"functions": false, "classes": false}],
+    "semi": [2, "never"],
+    "new-cap": [2, {"newIsCap": true, "capIsNew": false, "properties": false}],
+    "padded-blocks": 0,
+    "arrow-body-style": 0,
+    "no-underscore-dangle": [2, { "allowAfterThis": true, "allow": ["_id"] }],
+    "no-param-reassign": ["error", { "props": false }]
+  }
+}
